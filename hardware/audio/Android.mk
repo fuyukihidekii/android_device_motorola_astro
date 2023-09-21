fcfb64a653f43608063ab94230ee2b38f3cd7aea
豪 樹 冬 木 <fuyukihidekii@gmail.com>
2023-09-21 21:41:53 +0000

    astro: build audio HAL
    * Audio @ 6.0
    * SoundTrigger @ 2.2
    
    Signed-off-by: 豪 樹 冬 木 <fuyukihidekii@gmail.com>

---
+++ hardware/audio/Android.mk
@@ -0,0 +1,6 @@
+# Audio HAL
+PRODUCT_PACKAGES += \
+	android.hardware.audio@6.0-impl \
+	android.hardware.audio.effect@6.0-impl \
+	android.hardware.soundtrigger@2.2-impl \
+	android.hardware.audio.service
